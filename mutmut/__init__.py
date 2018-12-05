2c7bf79228ae79a26dcfa07a6784e9e6d391104f
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-05 14:22:18 -0700

    cleaning `__init__`

--- mutmut/__init__.py
+++ mutmut/__init__.py
@@ -1,15 +1,5 @@
-# coding=utf-8
-
-import sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 __version__ = '1.0.1'
-
-ALL = ('all', -1)
-
-
-if sys.version_info < (3, 0):   # pragma: no cover (python 2 specific)
-    # noinspection PyUnresolvedReferences
-    text_types = (str, unicode)
-else:
-    text_types = (str,)
 
