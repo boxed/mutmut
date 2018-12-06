d1a1e96643ed93ed6418cfa61a525c66f23fce14
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-06 00:10:52 -0700

    refactoring tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
+import time
 
 import pytest
 
 from mutmut.__main__ import main, python_source_files
+from mutmut.runner import popen_streaming_output
 
 in_travis = os.environ['PATH'].startswith('/home/travis/')
 
@@ -18,6 +19,7 @@ file_to_mutate_lines = [
     "d = dict(e=f)",
     "g: int = 2",
 ]
+
 file_to_mutate_contents = '\n'.join(file_to_mutate_lines) + '\n'
 
 test_file_contents = '''
@@ -35,21 +37,16 @@ def test_foo():
 
 
 @pytest.fixture
-def filesystem():
-    shutil.rmtree('test_fs', ignore_errors=True)
-    os.mkdir('test_fs')
-    with open('test_fs/foo.py', 'w') as f:
-        f.write(file_to_mutate_contents)
+def filesystem(tmpdir):
+    foo = tmpdir.mkdir("test_fs").join("foo.py")
+    foo.write(file_to_mutate_contents)
 
-    os.mkdir('test_fs/tests')
-    with open('test_fs/tests/test_foo.py', 'w') as f:
-        f.write(test_file_contents)
+    test_foo = tmpdir.mkdir(os.path.join("test_fs", "tests")).join("test_foo.py")
+    test_foo.write(test_file_contents)
 
-    os.chdir('test_fs')
+    os.chdir(tmpdir.join('test_fs'))
     yield
     os.chdir('..')
-    shutil.rmtree('test_fs')
-
     # This is a hack to get pony to forget about the old db file
     import mutmut.cache
     mutmut.cache.db.provider = None
@@ -57,41 +54,19 @@ def filesystem():
 
 
 @pytest.mark.usefixtures('filesystem')
-def test_full_run_no_surviving_mutants():
+def test_full_run_no_surviving_mutants(capsys):
     main(['--paths-to-mutate=foo.py'])
-
-
-#     assert u"""
-# To apply a mutant on disk:
-#     mutmut apply <id>
-#
-# To show a mutant:
-#     mutmut show <id>
-# """.strip() == result.output.strip()
+    captured = capsys.readouterr()
+    assert "ALIVE:     1" not in captured.out
 
 
 @pytest.mark.usefixtures('filesystem')
-def test_full_run_one_surviving_mutant():
+def test_full_run_one_surviving_mutant(capsys):
     with open('tests/test_foo.py', 'w') as f:
         f.write(test_file_contents.replace('assert foo(2, 2) is False\n', ''))
     main(['--paths-to-mutate=foo.py'])
-
-
-#     print(repr(result.output))
-#     assert u"""
-# To apply a mutant on disk:
-#     mutmut apply <id>
-#
-# To show a mutant:
-#     mutmut show <id>
-#
-#
-# Survived 🙁 (1)
-#
-# ---- foo.py (1) ----
-#
-# 1
-# """.strip() == result.output.strip()
+    captured = capsys.readouterr()
+    assert "ALIVE:     1" in captured.out
 
 
 @pytest.mark.usefixtures('filesystem')
@@ -100,11 +75,13 @@ def test_python_source_files():
     assert list(python_source_files('.', [])) == ['./foo.py', './tests/test_foo.py']
     assert list(python_source_files('.', ['./tests'])) == ['./foo.py']
 
-# @pytest.mark.skipif(in_travis, reason='This test does not work on TravisCI')
-# def test_timeout():
-#     start = datetime.now()
-#
-#     with pytest.raises(TimeoutError):
-#         popen_streaming_output('python -c "import time; time.sleep(4)"', lambda line: line, timeout=0.1)
-#
-#     assert (datetime.now() - start).total_seconds() < 3
+
+@pytest.mark.skipif(in_travis, reason='This test does not work on TravisCI')
+def test_timeout():
+    start = time.time()
+    with pytest.raises(TimeoutError):
+        popen_streaming_output(
+            'python -c "import time; time.sleep(4)"',
+            lambda line: line, timeout=0.1
+        )
+    assert (time.time() - start) < 3
