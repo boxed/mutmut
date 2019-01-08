17ec0b71f5fff339c74ed4c7e7639d735ea89f35
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-08 09:16:15 -0700

    wrapping mutation creation errors

--- mutmut/__main__.py
+++ mutmut/__main__.py
@@ -520,9 +520,8 @@ def add_mutations_by_file(mutations_by_file, filename, exclude, dict_synonyms):
     try:
         mutations_by_file[filename] = list_mutations(context)
         register_mutants(mutations_by_file)
-    except Exception:
-        print('Failed while creating mutations for %s, for line "%s"' % (context.filename, context.current_source_line))
-        raise
+    except Exception as e:
+        raise RuntimeError('Failed while creating mutations for %s, for line "%s"' % (context.filename, context.current_source_line), e)
 
 
 def coverage_exclude_callback(context, use_coverage, coverage_data):
