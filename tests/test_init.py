636cf67e41e6f59ebe9554aecedb9fc6788f8a85
Savo Kovacevic <savo.s.kovacevic@gmail.com>
2019-01-17 21:48:40 +0100

    Add some test cases for `__init__.py`, add `__repr__` to list of excluded lines in coverage

---
+++ tests/test_init.py
@@ -0,0 +1,26 @@
+from pytest import raises
+from mutmut import (
+    partition_node_list,
+    name_mutation,
+    Context,
+    ASTPattern
+)
+
+
+def test_partition_node_list_no_nodes():
+    with raises(AssertionError):
+        partition_node_list([], None)
+
+
+def test_name_mutation_simple_mutants():
+    assert name_mutation(None, 'True') == 'False'
+
+
+def test_context_exclude_line():
+    context = Context(
+        source="__import__('pkg_resources').declare_namespace(__name__)\n"
+    )
+    assert context.exclude_line() is True
+
+    context = Context(source="__all__ = ['hi']\n")
+    assert context.exclude_line() is True
