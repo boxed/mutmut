3e57e7ae304a122f1ad72e12c07aad4e7b8e80c1
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-22 17:50:09 -0700

    adding docstring

--- mutmut/__main__.py
+++ mutmut/__main__.py
@@ -598,6 +598,19 @@ def coverage_exclude_callback(context, use_coverage, coverage_data):
 
 
 def python_source_files(path, tests_dirs):
+    """Attempt to guess where the python source files to mutate are and yield
+    their paths
+
+    :param path: path to a python source file or package directory
+    :type path: str
+
+    :param tests_dirs: list of directory paths containing test files
+        (we do not want to mutate these!)
+    :type tests_dirs: list[str]
+
+    :return: generator listing the paths to the python source files to mutate
+    :rtype: Generator[str, None, None]
+    """
     if isdir(path):
         for root, dirs, files in os.walk(path):
             dirs[:] = [d for d in dirs if os.path.join(root, d) not in tests_dirs]
