13d28989973ca9263789d4f2f61816467c38f2ac
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-30 00:26:35 -0700

    refactoring

--- mutmut/cache.py
+++ mutmut/cache.py
@@ -314,7 +314,8 @@ def cached_mutation_status(filename, mutation_id, hash_of_tests):
     mutant = Mutant.get(line=line, index=mutation_id.index)
 
     if mutant.status == OK_KILLED:
-        # We assume that if a mutant was killed, a change to the test suite will mean it's still killed
+        # We assume that if a mutant was killed,
+        # a change to the test suite will mean it's still killed
         return OK_KILLED
 
     if mutant.tested_against_hash != hash_of_tests:
