bf5be8c51ebd2db26611a32c90096ca35749587b
edementev <e.dementyev@cian.ru>
2019-04-17 14:07:49 +0300

    added paths_to_exclude config param

--- tests/test_main.py
+++ tests/test_main.py
@@ -140,6 +140,20 @@ def test_python_source_files(expected, source_path, tests_dirs, filesystem):
     assert list(python_source_files(source_path, tests_dirs)) == expected
 
 
+def test_python_source_files__with_paths_to_exclude(fs):
+    paths_to_exclude = ['entities*']
+
+    fs.create_file('project/services/entities.py')
+    fs.create_file('project/services/main.py')
+    fs.create_file('project/services/utils.py')
+    fs.create_file('project/entities/user.py')
+
+    assert set(python_source_files('project', [], paths_to_exclude)) == {
+        'project/services/main.py',
+        'project/services/utils.py',
+    }
+
+
 def test_popen_streaming_output_timeout():
     start = time()
     with pytest.raises(TimeoutError):
