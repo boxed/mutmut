5c182510009befb2b12efe845fa40d528cf663a7
Nathan Klapstein <nklapste@ualberta.ca>
2019-07-11 17:19:32 -0400

    using `raise` instead of `exit`

--- mutmut/cache.py
+++ mutmut/cache.py
@@ -320,8 +320,7 @@ def mutation_id_from_pk(pk):
 def filename_and_mutation_id_from_pk(pk):
     mutant = Mutant.get(id=pk)
     if mutant is None:
-        print('Invalid mutation id {}'.format(pk))
-        exit(16)
+        raise ValueError("Obtained null mutant for pk: {}".format(pk))
     return mutant.line.sourcefile.filename, mutation_id_from_pk(pk)
 
 
