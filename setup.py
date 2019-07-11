c3556c8c38e2a5aaf1a29b136e2bb00390d05dbd
Nathan Klapstein <nklapste@ualberta.ca>
2019-07-10 20:14:27 -0400

    removing python2.7 build components

--- setup.py
+++ setup.py
@@ -110,8 +110,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
