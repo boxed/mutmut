ce6bdefcbef1bf98c440cdab69ab4813bed344cd
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-09-12 13:09:17 -0500

    Fix grammatical error in output legend.

--- mutmut/__main__.py
+++ mutmut/__main__.py
@@ -240,7 +240,7 @@ Legend for output:
 🎉 Killed mutants.   The goal is for everything to end up in this bucket.
 ⏰ Timeout.          Test suite took 10 times as long as the baseline so were killed.
 🤔 Suspicious.       Tests took a long time, but not long enough to be fatal.
-🙁 Survived.         This means your tests needs to be expanded.
+🙁 Survived.         This means your tests need to be expanded.
 🔇 Skipped.          Skipped.
 """)
     if runner is DEFAULT_RUNNER:
