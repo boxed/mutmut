2555d43fb182152fc4a7eb7167e3e26a1a66b825
Jan Królikowski <yanekk86@gmail.com>
2023-01-28 17:34:48 +0100

    Fix issue with empty file patches + tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,7 @@
 
+from pathlib import Path
 from time import sleep
-from pytest import raises
+from pytest import raises, fixture
 from unittest.mock import MagicMock, patch
 
 from mutmut import (
@@ -8,6 +9,8 @@ from mutmut import (
     name_mutation,
     run_mutation_tests,
     check_mutants,
+    close_active_queues,
+    read_patch_data,
     OK_KILLED,
     Context, 
     mutate)
@@ -73,3 +76,95 @@ def test_run_mutation_tests_thread_synchronization(monkeypatch):
 
     # assert
     assert progress_mock.registered_mutants == total_mutants
+
+    close_active_queues()
+
+@fixture
+def testpatches_path(testdata: Path):
+    return testdata / "test_patches"
+
+def test_read_patch_data_new_empty_file_not_in_the_list(testpatches_path: Path):
+    # arrange
+    new_empty_file_name = "new_empty_file.txt"
+    new_empty_file_patch = testpatches_path / "add_empty_file.patch"
+
+    # act
+    new_empty_file_changes = read_patch_data(new_empty_file_patch)
+
+    # assert
+    assert not new_empty_file_name in new_empty_file_changes
+
+def test_read_patch_data_removed_empty_file_not_in_the_list(testpatches_path: Path):
+    # arrange
+    existing_empty_file_name = "existing_empty_file.txt"
+    remove_empty_file_patch = testpatches_path / "remove_empty_file.patch"
+
+    # act
+    remove_empty_file_changes = read_patch_data(remove_empty_file_patch)
+
+    # assert
+    assert existing_empty_file_name not in remove_empty_file_changes
+
+def test_read_patch_data_renamed_empty_file_not_in_the_list(testpatches_path: Path):
+    # arrange
+    renamed_empty_file_name = "renamed_existing_empty_file.txt"
+    renamed_empty_file_patch = testpatches_path / "renamed_empty_file.patch"
+
+    # act
+    renamed_empty_file_changes = read_patch_data(renamed_empty_file_patch)
+
+    # assert
+    assert renamed_empty_file_name not in renamed_empty_file_changes
+
+def test_read_patch_data_added_line_is_in_the_list(testpatches_path: Path):
+    # arrange
+    file_name = "existing_file.txt"
+    file_patch = testpatches_path / "add_new_line.patch"
+
+    # act
+    file_changes = read_patch_data(file_patch)
+
+    # assert
+    assert file_name in file_changes
+    assert file_changes[file_name] == {3} # line is added between second and third
+
+def test_read_patch_data_edited_line_is_in_the_list(testpatches_path: Path):
+    # arrange
+    file_name = "existing_file.txt"
+    file_patch = testpatches_path / "edit_existing_line.patch"
+
+    # act
+    file_changes = read_patch_data(file_patch)
+
+    # assert
+    assert file_name in file_changes
+    assert file_changes[file_name] == {2} # line is added between 2nd and 3rd
+
+def test_read_patch_data_renamed_file_edited_line_is_in_the_list(testpatches_path: Path):
+    # arrange
+    original_file_name = "existing_file.txt"
+    new_file_name = "renamed_existing_file.txt"
+    file_patch = testpatches_path / "edit_existing_renamed_file_line.patch"
+
+    # act
+    file_changes = read_patch_data(file_patch)
+
+    # assert
+    assert original_file_name not in file_changes
+    assert new_file_name in file_changes
+    assert file_changes[new_file_name] == {3} # 3rd line is edited
+
+def test_read_patch_data_mutliple_files(testpatches_path: Path):
+    # arrange
+    expected_changes = {
+        "existing_file.txt": {2, 3},
+        "existing_file_2.txt": {4, 5},
+        "new_file.txt": {1, 2, 3}
+    }
+    file_patch = testpatches_path / "multiple_files.patch"
+
+    # act
+    actual_changes = read_patch_data(file_patch)
+
+    # assert
+    assert actual_changes == expected_changes
