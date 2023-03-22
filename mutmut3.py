8da0826de6be790e8e4b885e44561b5fb2aed06b
Anders Hovmöller <anders.hovmoller@dryft.se>
2023-03-22 13:08:31 +0100

    Reenabled **, * and / mutations. Mutmut3 now survives invalid syntax mutants and just skips over them.

--- mutmut3.py
+++ mutmut3.py
@@ -3,7 +3,6 @@ import gc
 import json
 import os
 from collections import defaultdict
-from dataclasses import dataclass
 from datetime import datetime
 from os import (
     makedirs,
@@ -12,17 +11,14 @@ from os import (
 from pathlib import Path
 from typing import Dict
 
-from mutmut import (
-    guess_paths_to_mutate,
-    list_mutations,
-    Context,
-    mutate,
-)
-
 from tqdm import tqdm
 
-from inspect import signature
-
+from mutmut import (
+    Context,
+    guess_paths_to_mutate,
+    list_mutations,
+    mutate,
+)
 
 _stats = set()
 
@@ -50,19 +46,17 @@ def walk_files():
 
 
 def create_mutants():
-    function_by_id = {}
     for path in walk_files():
-        create_mutants_for_file(path, function_by_id=function_by_id)
-    return function_by_id
+        create_mutants_for_file(path)
 
 
-def write_trampoline(out, orig_name, mutant_names):
+def write_trampoline(out, orig_name, mutant_names, invalid_mutants):
     print(file=out)
 
     def index(s):
         return s.rpartition('_')[-1]
 
-    print(f'{orig_name}_mutants = {{' + ', '.join(f'{index(m)}: {m}' for m in mutant_names) + '}', file=out)
+    print(f'{orig_name}_mutants = {{' + ', '.join(f'{index(m)}: {m}' for m in mutant_names if m not in invalid_mutants) + '}', file=out)
 
     print(file=out)
     print(f"""
@@ -73,23 +67,35 @@ def {orig_name}(*args, **kwargs):
     print(f'{orig_name}.__signature__ = __signature({orig_name}_orig)\n', file=out)
 
 
+class InvalidMutantException(Exception):
+    pass
+
+
 def write_mutant(out, c, mutation_id, next_id, mutant_names, orig_name):
     if not mutation_id.subject:
         return
-    c.mutation_id = mutation_id
-    new_code, number = mutate(c)
-    node = mutation_id.subject
-    node.name.value += f'_mutant_{next_id}'
-    mutant_names.append(node.name.value)
-    # assert number == 1, number
-    if number != 1:
-        print(f'warning: got {number} mutations when mutating {mutation_id}')
-    code = node.get_code()
-    print(code.strip(), file=out)
-    print(file=out)
-    print(file=out)
-    node.name.value = orig_name
-    return node.get_code()
+    try:
+        c.mutation_id = mutation_id
+        new_code, number = mutate(c)
+        node = mutation_id.subject
+        node.name.value += f'_mutant_{next_id}'
+        mutant_names.append(node.name.value)
+        # assert number == 1, number
+        if number != 1:
+            print(f'warning: got {number} mutations when mutating {mutation_id}')
+        code = node.get_code()
+
+        try:
+            ast.parse(code)
+        except (IndentationError, SyntaxError):
+            raise InvalidMutantException()
+
+        print(code.strip(), file=out)
+        print(file=out)
+        print(file=out)
+        return node.get_code()
+    finally:
+        node.name.value = orig_name
 
 
 def write_original_alias(out, last_subject):
@@ -121,7 +127,7 @@ def trampoline(orig, mutants, *args, **kwargs):
 """, file=out)
 
 
-def create_mutants_for_file(filename, function_by_id):
+def create_mutants_for_file(filename):
     output_path = Path('mutants') / filename
     makedirs(output_path.parent, exist_ok=True)
 
@@ -131,7 +137,9 @@ def create_mutants_for_file(filename, function_by_id):
         # print('    skipped', output_path, 'already up to date')
         return
 
-    num_mutants_by_function = {}
+    num_mutants_by_function = defaultdict(int)
+
+    invalid_mutants = set()
 
     with open(output_path, 'w') as out:
         c = Context(filename=filename)
@@ -147,7 +155,6 @@ def create_mutants_for_file(filename, function_by_id):
         last_code = None
 
         next_id = 0
-
         for mutation_id in mutation_ids:
             if not mutation_id.subject:
                 continue
@@ -161,27 +168,32 @@ def create_mutants_for_file(filename, function_by_id):
                     write_original_alias(out, last_subject)
 
                 if mutant_names:
-                    write_trampoline(out, orig_name, mutant_names)
+                    write_trampoline(out, orig_name, mutant_names, invalid_mutants)
 
                 orig_name = mutation_id.subject.name.value
                 mutant_names = []
                 last_subject = mutation_id.subject
                 next_id = 0
 
-            code = write_mutant(out, c, mutation_id, next_id, mutant_names, orig_name)
+            next_id += 1
+
+            try:
+                code = write_mutant(out, c, mutation_id, next_id, mutant_names, orig_name)
+            except InvalidMutantException:
+                invalid_mutants.add(f'{mutation_id.subject.name.value}_mutant_{next_id}')
+                continue
+
             module_name = str(filename)[:-len(filename.suffix)].replace(os.sep, ".").replace('.__init__', '')
             assert last_code != code
             last_code = code
 
-            num_mutants_by_function[f'{module_name}.{orig_name}'] = next_id
-
-            next_id += 1
+            num_mutants_by_function[f'{module_name}.{orig_name}'] += 1
 
         if last_subject:
             write_original_alias(out, last_subject)
 
         if mutant_names:
-            write_trampoline(out, orig_name, mutant_names)
+            write_trampoline(out, orig_name, mutant_names, invalid_mutants)
 
     # validate no syntax errors
     with open(output_path) as f:
@@ -196,6 +208,7 @@ def create_mutants_for_file(filename, function_by_id):
         json.dump(dict(
             num_mutants_by_function=num_mutants_by_function,
             result_by_key={},
+            invalid_mutants=list(invalid_mutants),
         ), f)
 
     os.utime(output_path, (input_stat.st_atime, input_stat.st_mtime))
@@ -210,6 +223,7 @@ class MutationData:
         self.key_by_pid = {}
         self.result_by_key = self.meta.pop('result_by_key')
         self.num_mutants_by_function = self.meta.pop('num_mutants_by_function')
+        self.invalid_mutants = set(self.meta.pop('invalid_mutants'))
         assert not self.meta, self.meta  # We should read all the data!
 
     def register_pid(self, *, pid, key):
@@ -225,6 +239,7 @@ class MutationData:
             json.dump(dict(
                 result_by_key=self.result_by_key,
                 num_mutants_by_function=self.num_mutants_by_function,
+                invalid_mutants=list(self.invalid_mutants),
             ), f)
 
 
@@ -243,25 +258,7 @@ def mutmut_3():
 
     sys.path.insert(0, os.path.abspath('mutants'))
 
-    # TODO: run these three steps only if we have mutants to test
-    print('running baseline...')
-    os.environ['MUTANT_UNDER_TEST'] = '..match nothing..'
-    if hammett.main(quiet=True, fail_fast=True, disable_assert_analyze=True, use_cache=False) != 0:
-        print("FAILED")
-        return
-    print('done')
-
-    print('running forced fail test')
-    os.environ['MUTANT_UNDER_TEST'] = 'fail'
-    try:
-        if hammett.main(quiet=True, fail_fast=True, disable_assert_analyze=True, use_cache=False) == 0:
-            print("FAILED")
-            return
-    except Exception as e:
-        # We get here if there's a mutant in the setup code path
-        assert e.args[0] == 'Failed programmatically'
-    print('done')
-
+    # TODO: run these steps only if we have mutants to test
     print('running stats...')
     os.environ['MUTANT_UNDER_TEST'] = 'stats'
 
@@ -275,6 +272,17 @@ def mutmut_3():
     if hammett.main(quiet=True, fail_fast=True, disable_assert_analyze=True, post_test_callback=post_test_callback, use_cache=False) != 0:
         print("FAILED")
         return
+    print('done')
+
+    print('running forced fail test')
+    os.environ['MUTANT_UNDER_TEST'] = 'fail'
+    try:
+        if hammett.main(quiet=True, fail_fast=True, disable_assert_analyze=True, use_cache=False) == 0:
+            print("FAILED")
+            return
+    except Exception as e:
+        # We get here if there's a mutant in the setup code path
+        assert e.args[0] == 'Failed programmatically'
     print('done')
 
     def read_one_child_exit_status():
@@ -311,6 +319,9 @@ def mutmut_3():
                     key = f'{function}${i}'
 
                     if key in m.result_by_key:
+                        continue
+
+                    if key in m.invalid_mutants:
                         continue
 
                     pid = os.fork()
