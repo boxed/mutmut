d385206fa36f5b1ee528ea5dcad331f29595f134
Inte Vleminckx <inte.vleminckx@student.uantwerpen.be>
2024-05-10 11:34:48 +0200

    refactor: Divided the mutator functions in a Mutator class and a Mutator Helper class

---
+++ mutmut/mutator/mutator.py
@@ -0,0 +1,179 @@
+from io import open
+from typing import Tuple
+
+from parso import parse
+
+from mutmut.helpers.context import Context, ALL
+
+try:
+    import mutmut_config
+except ImportError:
+    mutmut_config = None
+
+from mutmut.mutator.mutator_helper import MutatorHelper
+
+
+class SkipException(Exception):
+    pass
+
+
+class Mutator:
+
+    def __init__(self, context: Context):
+        self.context = context
+        self.helper = MutatorHelper()
+
+    def mutate(self) -> Tuple[str, int]:
+        """
+        :return: tuple of mutated source code and number of mutations performed
+        """
+        try:
+            result = parse(self.context.source, error_recovery=False)
+        except Exception:
+            print('Failed to parse {}. Internal error from parso follows.'.format(self.context.filename))
+            print('----------------------------------')
+            raise
+        self.mutate_list_of_nodes(result)
+        mutated_source = result.get_code().replace(' not not ', ' ')
+        if self.context.remove_newline_at_end:
+            assert mutated_source[-1] == '\n'
+            mutated_source = mutated_source[:-1]
+
+        # If we said we mutated the code, check that it has actually changed
+        if self.context.performed_mutation_ids:
+            if self.context.source == mutated_source:
+                raise RuntimeError(
+                    "Mutation context states that a mutation occurred but the "
+                    "mutated source remains the same as original")
+        self.context.mutated_source = mutated_source
+        return mutated_source, len(self.context.performed_mutation_ids)
+
+    def mutate_node(self, node):
+        self.context.stack.append(node)
+        try:
+
+            if self.helper.is_special_node(node):
+                return
+
+            if self.helper.is_dynamic_import_node(node):
+                return
+
+            if self.helper.should_update_line_index(node, self.context):
+                self.context.current_line_index = node.start_pos[0] - 1
+                self.context.index = 0  # indexes are unique per line, so start over here!
+
+            if self.helper.is_a_dunder_whitelist_node(node):
+                return
+
+            # Avoid mutating pure annotations
+            if self.helper.is_pure_annotation(node):
+                return
+
+            if hasattr(node, 'children'):
+                self.mutate_list_of_nodes(node)
+
+                # this is just an optimization to stop early
+                if self.stop_early():
+                    return
+
+            mutation = self.helper.mutations_by_type.get(node.type)
+
+            if mutation is None:
+                return
+
+            self.process_mutations(node, mutation)
+
+        finally:
+            self.context.stack.pop()
+
+    def get_old_and_new_mutation_instance(self, node, node_attribute, concrete_mutation):
+        old = getattr(node, node_attribute)
+
+        mutation_instance = concrete_mutation()
+
+        new = mutation_instance.mutate(
+            context=self.context,
+            node=node,
+            value=getattr(node, 'value', None),
+            children=getattr(node, 'children', None),
+        )
+
+        return old, new
+
+    def process_mutations(self, node, mutation):
+        for node_attribute, concrete_mutation in sorted(mutation.items()):
+            if self.context.exclude_line():
+                continue
+
+            old, new = self.get_old_and_new_mutation_instance(node, node_attribute, concrete_mutation)
+
+            new_list = self.helper.wrap_or_return_mutation_instance(new, old)
+
+            is_optimized = self.alternate_mutations(new_list, old, node, node_attribute)
+
+            if is_optimized:
+                return
+
+    def alternate_mutations(self, new_list, old, node, node_attribute):
+        # go through the alternate mutations in reverse as they may have
+        # adverse effects on subsequent mutations, this ensures the last
+        # mutation applied is the original/default/legacy mutmut mutation
+        for new in reversed(new_list):
+            assert not callable(new)
+
+            self.apply_mutation_and_update_context(new, old, node, node_attribute)
+
+            # this is just an optimization to stop early
+            if self.stop_early():
+                return True
+
+        return False
+
+    def apply_mutation_and_update_context(self, new, old, node, node_attribute):
+        if new is None or new == old:
+            self.context.index += 1
+            return
+
+        if hasattr(mutmut_config, 'pre_mutation_ast'):
+            mutmut_config.pre_mutation_ast(context=self.context)
+
+        if self.context.should_mutate(node):
+            self.context.performed_mutation_ids.append(self.context.mutation_id_of_current_index)
+            setattr(node, node_attribute, new)
+
+        self.context.index += 1
+
+    def mutate_list_of_nodes(self, node):
+        return_annotation_started = False
+
+        for child_node in node.children:
+
+            return_annotation_started = self.helper.get_return_annotation_started(child_node, return_annotation_started)
+
+            if return_annotation_started:
+                continue
+
+            self.mutate_node(child_node)
+
+            # this is just an optimization to stop early
+            if self.stop_early():
+                return
+
+    def stop_early(self):
+        return self.context.performed_mutation_ids and self.context.mutation_id != ALL
+
+    def list_mutations(self):
+        assert self.context.mutation_id == ALL
+        self.mutate()
+        return self.context.performed_mutation_ids
+
+    def mutate_file(self, backup: bool) -> Tuple[str, str]:
+        with open(self.context.filename) as f:
+            original = f.read()
+        if backup:
+            with open(self.context.filename + '.bak', 'w') as f:
+                f.write(original)
+        mutated, _ = self.mutate()
+        with open(self.context.filename, 'w') as f:
+            f.write(mutated)
+        return original, mutated
---
+++ mutmut/mutator/mutator_helper.py
@@ -0,0 +1,110 @@
+from mutmut.mutations.and_or_test_mutation import AndOrTestMutation
+from mutmut.mutations.argument_mutation import ArgumentMutation
+from mutmut.mutations.decorator_mutation import DecoratorMutation
+from mutmut.mutations.expression_mutation import ExpressionMutation
+from mutmut.mutations.f_string_mutation import FStringMutation
+from mutmut.mutations.keyword_mutation import KeywordMutation
+from mutmut.mutations.lambda_mutation import LambdaMutation
+from mutmut.mutations.name_mutation import NameMutation
+from mutmut.mutations.number_mutation import NumberMutation
+from mutmut.mutations.operator_mutation import OperatorMutation
+from mutmut.mutations.string_mutation import StringMutation
+
+try:
+    import mutmut_config
+except ImportError:
+    mutmut_config = None
+
+
+class MutatorHelper:
+
+    def __init__(self):
+        self.dunder_whitelist = [
+            'all',
+            'version',
+            'title',
+            'package_name',
+            'author',
+            'description',
+            'email',
+            'version',
+            'license',
+            'copyright',
+        ]
+
+        self.newline = {'formatting': [], 'indent': '', 'type': 'endl', 'value': ''}
+
+        self.mutations_by_type = {
+            'operator': dict(value=OperatorMutation),
+            'keyword': dict(value=KeywordMutation),
+            'number': dict(value=NumberMutation),
+            'name': dict(value=NameMutation),
+            'string': dict(value=StringMutation),
+            'fstring': dict(children=FStringMutation),
+            'argument': dict(children=ArgumentMutation),
+            'or_test': dict(children=AndOrTestMutation),
+            'and_test': dict(children=AndOrTestMutation),
+            'lambdef': dict(children=LambdaMutation),
+            'expr_stmt': dict(children=ExpressionMutation),
+            'decorator': dict(children=DecoratorMutation),
+            'annassign': dict(children=ExpressionMutation),
+        }
+
+    def is_a_dunder_whitelist_node(self, node):
+        if node.type != 'expr_stmt':
+            return False
+
+        if node.childeren[0].type != 'name':
+            return False
+
+        if not node.children[0].value.startswith('__'):
+            return False
+
+        if not node.children[0].value.endswith('__'):
+            return False
+
+        return node.children[0].value[2:-2] in self.dunder_whitelist
+
+    def is_return_annotation_start(self, node):
+        return self.check_node_type_and_value(node, 'operator', '->')
+
+    def is_return_annotation_end(self, node):
+        return self.check_node_type_and_value(node, 'operator', ':')
+
+    def get_return_annotation_started(self, node, return_annotation_started):
+        if self.is_return_annotation_start(node):
+            return_annotation_started = True
+
+        if return_annotation_started and self.is_return_annotation_end(node):
+            return_annotation_started = False
+
+        return return_annotation_started
+
+    @staticmethod
+    def is_special_node(node):
+        return node.type in ('tfpdef', 'import_from', 'import_name')
+
+    @staticmethod
+    def is_dynamic_import_node(node):
+        return node.type == 'atom_expr' and node.children and node.children[0].type == 'name' and node.children[
+            0].value == '__import__'
+
+    @staticmethod
+    def should_update_line_index(node, context):
+        return node.start_pos[0] - 1 != context.current_line_index
+
+    @staticmethod
+    def is_pure_annotation(node):
+        return node.type == 'annassign' and len(node.children) == 2
+
+    @staticmethod
+    def wrap_or_return_mutation_instance(new, old):
+        if isinstance(new, list) and not isinstance(old, list):
+            # multiple mutations
+            return new
+
+        return [new]
+
+    @staticmethod
+    def check_node_type_and_value(node, type, value):
+        return node.type == type and node.value == value
