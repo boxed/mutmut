43a4bdd8b491655ecb9f36414967f0ce0fe1e378
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2025-01-19 00:27:04 +0530

    Clutching at straws

--- mutmut/__main__.py
+++ mutmut/__main__.py
@@ -891,7 +891,7 @@ def run_forced_fail_test(runner):
         try:
             print(f"runner.run_forced_fail(): {runner.run_forced_fail()}")
             if runner.run_forced_fail() == 0:
-                catcher.dump_output()
+                # catcher.dump_output()
                 print("FAILED: Unable to force test failures")
                 raise SystemExit(1)
         except MutmutProgrammaticFailException:
