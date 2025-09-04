4bcf3bcb639c138111f090d1dc95f537a5b10154
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-09-04 18:23:22 +1000

    Verify wonkiness when mutant_names tuple includes at least 1 space
    Further discussion with @boxed revealed that I hadn't considered
    the case of a _multiple-element_ mutant_names tuple where at
    least one of the elements contains at least one space.
    
    First step, verify the wonkiness exists.

--- tests/e2e/test_e2e_result_filter.py
+++ tests/e2e/test_e2e_result_filter.py
@@ -65,21 +65,62 @@ def asserts_handled_mutant_spec(project: str, filter_mutants: str):
     assert no_exception, str(exception_type)
 
 
-def test_my_lib_result_filter_singleton_mutant_explicit():
+def asserts_handled_mutant_spec_extra(project: str, tuple_first: str, filter_mutants: str):
+    """Runs mutmut on this project and verifies that it was able to handle the supplied mutant spec."""
+    project_path = Path("..").parent / "e2e_projects" / project
+
+    mutants_path = project_path / "mutants"
+    shutil.rmtree(mutants_path, ignore_errors=True)
+
+    # Work out a lower bound on the number of mutants that _should_ have run
+    min_count = 2 + tuple_first.count(' ') + filter_mutants.count(' ')
+
+    # mutmut run
+    no_exception = True
+    exception_type = None
+    try:
+        with change_cwd(project_path):
+            mut_names = tuple([tuple_first, filter_mutants])
+            _run(mut_names, None)
+    except Exception as e:
+        no_exception = False
+        exception_type = e
+
+    assert no_exception, str(exception_type)
+
+    results = read_all_stats_for_project(project_path)
+    actual_count = 0
+    for item in results:
+        matches = [1 for v in list(results[item].values()) if isinstance(v, int) and 33 != v]
+        actual_count += sum(matches)
+
+    assert actual_count >= min_count,\
+        f"Test config expected at least {min_count} mutants run, only {actual_count} mutant was/were run"
+
+
+def test_config_result_filter_singleton_mutant_explicit():
     mutmut._reset_globals()
     asserts_handled_mutant_spec("config", "config_pkg.math.x_add__mutmut_1")
 
 
-def test_my_lib_result_filter_singleton_mutant_wildcard():
+def test_config_result_filter_singleton_mutant_wildcard():
     mutmut._reset_globals()
     asserts_handled_mutant_spec("config", "config_pkg.math.x_call_depth_two__mutmut_?")
 
 
-def test_my_lib_result_filter_doubleton_mutant_explicit():
+def test_config_result_filter_doubleton_mutant_explicit():
     mutmut._reset_globals()
     asserts_handled_mutant_spec("config", "config_pkg.math.x_call_depth_two__mutmut_1 config_pkg.math.x_add__mutmut_1")
 
 
-def test_my_lib_result_filter_doubleton_mutant_wildcard():
+def test_config_result_filter_doubleton_mutant_wildcard():
     mutmut._reset_globals()
     asserts_handled_mutant_spec("config", "config_pkg.math.x_call_depth_two__mutmut_? config_pkg.math.x_add__mutmut_?")
+
+
+def test_config_result_filter_tuple_plus_doubleton():
+    mutmut._reset_globals()
+    asserts_handled_mutant_spec_extra(
+        'config',
+        'config_pkg.x_hello__mutmut_1', 'config_pkg.math.x_call_depth_two__mutmut_? config_pkg.math.x_add__mutmut_?'
+    )
