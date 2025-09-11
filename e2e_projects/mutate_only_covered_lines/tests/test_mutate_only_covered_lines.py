b9bf346881e4314ab951fc49ea766c51b9b7154b
Lou Franco <lfranco@greenwave-solutions.com>
2025-09-11 11:05:36 -0400

    e2e test for when mutate_only_covered_lines = true

---
+++ e2e_projects/mutate_only_covered_lines/tests/test_mutate_only_covered_lines.py
@@ -0,0 +1,6 @@
+from mutate_only_covered_lines import hello_mutate_only_covered_lines
+
+"""This tests the mutate_only_covered_lines feature."""
+
+def test_mutate_only_covered_lines():
+    assert hello_mutate_only_covered_lines(True) == "Hello from mutate_only_covered_lines! (true)"
