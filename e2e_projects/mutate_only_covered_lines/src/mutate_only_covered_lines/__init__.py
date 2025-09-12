74ac2e8d26d47da5360fc8e9076d09a725ff7dc2
Lou Franco <lfranco@greenwave-solutions.com>
2025-09-12 10:53:15 -0400

    mutate_only_covered_lines  e2e test added to test multiline
    I inspected the file generated in mutants and the "Bar" on line 10 is not mutated, but coverage.py reported that it wasn't covered, so the bug is in coverage.py.

--- e2e_projects/mutate_only_covered_lines/src/mutate_only_covered_lines/__init__.py
+++ e2e_projects/mutate_only_covered_lines/src/mutate_only_covered_lines/__init__.py
@@ -3,3 +3,26 @@ def hello_mutate_only_covered_lines(simple_branch: bool) -> str:
         return "Hello from mutate_only_covered_lines! (true)"
     else:
         return "Hello from mutate_only_covered_lines! (false)"
+
+def mutate_only_covered_lines_multiline(simple_branch: bool) -> str:
+    x = (
+        "Foo"
+        "Bar" # coverage.py incorrectly reports this line as not covered. If that changes, tests will break
+    )
+    if simple_branch:
+        y = [
+            i * 2
+            for i in range(10)
+            if i % 2 == 0
+        ]
+        return f"Hello from mutate_only_covered_lines!" \
+            f" (true) {x} {y}"
+    else:
+        y = [
+            i * 2
+            for i in range(10)
+            if i % 2 == 0
+        ]
+        return f"Hello from mutate_only_covered_lines!" \
+            f" (false) {x} {y}"
+
