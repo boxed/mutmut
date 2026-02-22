9bfcc75dc9fc1523f51620b5b2ddaaa7a6091600
A_A <21040751+Otto-AA@users.noreply.github.com>
2026-02-22 17:10:00 +0100

    code cleanup

--- tests/e2e/test_e2e_type_checking.py
+++ tests/e2e/test_e2e_type_checking.py
@@ -7,18 +7,18 @@ def test_type_checking_result_snapshot():
     assert run_mutmut_on_project("type_checking") == snapshot(
         {
             "mutants/src/type_checking/__init__.py.meta": {
-                "type_checking.x_hello__mutmut_1": 6,
+                "type_checking.x_hello__mutmut_1": 37,
                 "type_checking.x_hello__mutmut_2": 1,
                 "type_checking.x_hello__mutmut_3": 1,
                 "type_checking.x_hello__mutmut_4": 1,
-                "type_checking.x_a_hello_wrapper__mutmut_1": 6,
+                "type_checking.x_a_hello_wrapper__mutmut_1": 37,
                 "type_checking.x_a_hello_wrapper__mutmut_2": 0,
-                "type_checking.xǁPersonǁset_name__mutmut_1": 6,
-                "type_checking.x_mutate_me__mutmut_1": 6,
-                "type_checking.x_mutate_me__mutmut_2": 6,
+                "type_checking.xǁPersonǁset_name__mutmut_1": 37,
+                "type_checking.x_mutate_me__mutmut_1": 37,
+                "type_checking.x_mutate_me__mutmut_2": 37,
                 "type_checking.x_mutate_me__mutmut_3": 1,
                 "type_checking.x_mutate_me__mutmut_4": 1,
-                "type_checking.x_mutate_me__mutmut_5": 6,
+                "type_checking.x_mutate_me__mutmut_5": 37,
             }
         }
     )
